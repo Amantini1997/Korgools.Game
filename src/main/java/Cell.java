8e24341ca0d8bff46e617f167d8e807f629beba7
Alessandro <ale.amantini1997@gmail.com>
2018-11-10 17:45:29 +0000

    Create content for class Cell
    Created fields and methods for class Cell:
     this class is gonna be a basic one that other classes can extend to
     make more specific

--- src/main/java/Cell.java
+++ src/main/java/Cell.java
@@ -1,4 +1,17 @@
-package main.java;
-
 public class Cell {
+	protected boolean isWhite;
+	protected int korgools;
+	
+	public Cell(boolean isWhite,int korgools){
+		this.isWhite = isWhite;
+		this.korgools = korgools;
+	}
+	
+	public boolean isWhite(){
+		return isWhite;
+	}
+
+	public int getKorgools(){	
+		return korgools;
+	}
 }
