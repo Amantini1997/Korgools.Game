dfb4375dc0fb3a9ee078d4d5baeb46080def614b
k1763585 <alessandro.amantini@kcl.ac.uk>
2018-12-12 20:47:00 +0000

    Add tests

---
+++ src/test/java/backend/AIBoardTest.java
@@ -0,0 +1,77 @@
+package backend;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AIBoardTest{
+  private AIBoard board;
+
+  // @Test
+  // public void makeAMove(){
+  //   board = new Board("0,0,0,0,0,0,0,0,0,13,0\n"+
+  //                     "0,0,1,0,0,0,0,0,0,20,0\nw");
+  //   board.makeAMove(2);
+  //   assertTrue(board.toString().equals("0,0,0,0,0,0,0,0,0,13,0\n"+
+  //                                      "0,0,1,0,0,0,0,0,0,20,0\nw"));
+  // }
+  @Test
+  public void AI1MakesAMoveWithoutTellingIt(){
+    board = new AIBoard("2,0,0,0,0,0,0,0,0,60,-1\n"+
+                        "2,0,0,0,0,0,0,0,0,81,-1\nw",1);
+    board.makeAMove(0);
+    assertEquals(board.toString(),"1,1,0,0,0,0,0,0,0,60,-1\n"+
+                                  "1,1,0,0,0,0,0,0,0,81,-1\nw");
+  }
+
+  @Test
+  public void AI2MakesAMoveWithoutTellingIt(){
+    board = new AIBoard("2,0,0,0,0,0,0,0,0,60,-1\n"+
+                        "2,0,0,0,0,0,0,0,0,81,-1\nw",2);
+    board.makeAMove(0);
+    assertEquals(board.toString(),"1,1,0,0,0,0,0,0,0,60,-1\n"+
+                                  "1,1,0,0,0,0,0,0,0,81,-1\nw");
+  }
+
+  @Test
+  public void AI3MakesAMoveWithoutTellingIt(){
+    board = new AIBoard("2,0,0,0,0,0,0,0,0,60,-1\n"+
+                        "2,0,0,0,0,0,0,0,0,81,-1\nw",3);
+    board.makeAMove(0);
+    assertEquals(board.toString(),"1,1,0,0,0,0,0,0,0,60,-1\n"+
+                                  "1,1,0,0,0,0,0,0,0,81,-1\nw");
+  }
+
+  @Test
+  public void AI3MakesTheRightMove(){
+    board = new AIBoard("0,1,1,3,1,0,8,3,1,65,-1\n"+
+                        "1,1,3,2,2,1,1,1,0,67,-1\nw",3);
+    board.makeAMove(2);
+    assertEquals(board.toString(),"0,1,1,3,1,0,1,4,2,69,-1\n"+
+                                  "2,2,2,4,0,1,1,1,0,67,-1\nw");
+  }
+
+  @Test
+  public void notAIPlayerWins(){
+    board = new AIBoard("1,0,0,0,0,0,0,0,0,60,0\n"+
+                        "0,0,0,0,0,0,0,0,1,81,-1\nw",2);
+    assertTrue(board.makeAMove(8));
+  }
+
+  @Test
+  public void AIWins(){
+    board = new AIBoard("0,0,0,0,0,0,0,0,3,81,-1\n"+
+                        "1,0,0,0,0,0,0,0,0,60,-1\nw",2);
+    assertTrue(board.makeAMove(0));
+  }
+
+  @Test
+  public void nooneWins(){
+    board = new AIBoard("1,0,0,0,0,0,0,0,0,81,-1\n"+
+                        "1,0,0,0,0,0,0,0,0,60,-1\nw",2);
+    assertFalse(board.makeAMove(0));
+  }
+}
--- src/test/java/backend/BoardTest.java
+++ src/test/java/backend/BoardTest.java
@@ -1,147 +1,65 @@
 package backend;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import org.junit.Test;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class BoardTest{
-  /*private Board board;
+  private Board board;
 
-  @Before
-  public void setUp(){
-    board = new Board();
+  // @Test
+  // public void makeAMove(){
+  //   board = new Board("0,0,0,0,0,0,0,0,0,13,0\n"+
+  //                     "0,0,1,0,0,0,0,0,0,20,0\nw");
+  //   board.makeAMove(2);
+  //   assertTrue(board.toString().equals("0,0,0,0,0,0,0,0,0,13,0\n"+
+  //                                      "0,0,1,0,0,0,0,0,0,20,0\nw"));
+  // }
+  @Test
+  public void playerHasAMove(){
+    board = new Board("0,0,0,0,0,0,0,0,0,60,-1\n"+
+                      "0,0,0,0,0,0,0,0,3,81,-1\nw");
+    assertTrue(board.currentPlayer.hasAMove());
   }
 
   @Test
-  public void testBoardToStringTrue() {
-    assertEquals(board.toString(), "9,9,9,9,9,9,9,9,9,0,-1\n"+
-                                   "9,9,9,9,9,9,9,9,9,0,-1\nw");
+  public void playerDoesntHaveAMove(){
+    board = new Board("0,0,0,0,0,0,0,0,0,60,-1\n"+
+                      "0,0,0,0,0,0,0,0,3,81,-1\nb");
+    assertFalse(board.currentPlayer.hasAMove());
   }
 
   @Test
-  public void testBoardToStringFalse() {
-    assertNotEquals(board.toString(), "9,9,9,9,9,9,9,9,9,0,-1\n"+
-                                      "9,9,9,9,9,9,9,9,9,0,-1\nb");
+  public void gameHasEndedShouldReturnFalse(){
+    board = new Board("0,0,0,0,0,0,0,0,0,81,0\n"+
+                      "0,0,0,0,0,0,0,1,0,81,0\nb");
+    assertFalse(board.makeAMove(7));
   }
 
   @Test
-  public void testBoardAfterFirstMoveToString() {
-    assertEquals(board.toString(), "9,9,9,9,9,9,9,9,9,0,-1\n"+
-                                   "9,9,9,9,9,9,9,9,9,0,-1\nw");
-
-    //board = new Board();
-    //board.makeAMove(8);
-    //assertEquals(board.toString(), "1,10,10,10,10,10,10,10,10,0,-1\n9,9,9,9,9,9,9,9,9,0,-1\nb");
+  public void gameHasEndedTestShouldReturnTrueForWhite(){
+    board = new Board("0,0,0,0,0,0,0,1,0,77,-1\n"+
+                      "0,0,0,0,0,0,0,0,3,81,0\nw");
+    assertFalse(board.gameHasEnded());
   }
 
   @Test
-  public void makeAMoveShouldStealKorgoolsEndingInAEvenHole(){
-    board.makeAMove(6);
-    assertEquals(board.toString(), "10,10,10,10,10,0,9,9,9,0,-1\n"+
-                                   "9,9,9,9,9,9,1,10,10,10,-1\nb");
+  public void gameHasEndedTestShouldReturnTrueForBlack(){
+    board = new Board("2,0,0,0,0,0,0,1,0,82,0\n"+
+                      "0,0,0,0,0,0,0,0,0,80,-1\nb");
+    assertTrue(board.gameHasEnded());
   }
 
   @Test
-  public void makeAMoveShouldntStealKorgoolsEndingInASamePlayerEvenHole(){
-    board.makeAMove(0);
-    board.makeAMove(0);
-    board.makeAMove(1);
-    board.makeAMove(0);
-    board.makeAMove(2);
-    board.makeAMove(0);
+  public void makeAMoveWithNoMoveShouldChangeTheTurn(){
+    board = new Board("0,1,0,0,0,0,0,0,0,79,-1\n"+
+                      "0,0,0,0,0,0,0,0,1,80,-1\nw");
     board.makeAMove(8);
-    assertEquals(board.toString(),"2,15,12,12,11,11,11,11,11,0,-1\n"+
-                                  "2,2,1,12,12,12,12,12,1,0,-1\nb");
+    board.makeAMove(0);
+    assertEquals(board.toString(),"0,2,0,0,0,0,0,0,0,79,-1\n"+
+                                  "0,0,0,0,0,0,0,0,0,80,-1\nb");
   }
-
-  @Test
-  public void makeAMoveOn1KorgoolTuzShouldEmptyIt(){
-    board.makeAMove(0);
-    board.makeAMove(0);
-    board.makeAMove(0);
-    assertEquals(board.toString(),"1,10,10,10,10,10,10,10,10,0,-1\n"+
-                                  "0,11,10,10,10,10,10,10,10,0,-1\nb");
-  }
-
-  @Test
-  public void makeAMoveOnAEmptyHoleShouldntChangePlayersTurn(){
-    board.makeAMove(0);
-    board.makeAMove(0);
-    board.makeAMove(0);
-    board.makeAMove(0);
-    board.makeAMove(0);
-    assertEquals(board.toString(),"0,11,10,10,10,10,10,10,10,0,-1\n"+
-                                  "0,11,10,10,10,10,10,10,10,0,-1\nw");
-  }
-
-  @Test
-  public void makeAMoveShouldCreateAtuz(){
-    board.makeAMove(4);
-    board.makeAMove(4);
-    board.makeAMove(5);
-    board.makeAMove(4);
-    board.makeAMove(6);
-    board.makeAMove(4);
-    board.makeAMove(0);
-    board.makeAMove(2);
-    assertEquals(board.toString(),"13,12,1,3,2,15,12,12,11,13,-1\n"+
-                                  "2,12,12,2,0,2,2,13,13,10,4\nw");
-  }backend.BoardTest > makeAMoveOnAEmptyHoleShouldntChangePlayersTurn FAILED
-    org.junit.ComparisonFailure at BoardTest.java:74
-
-
-  @Test
-  public void makeAMovePassingOverATuzShouldEmptyTheTuzEvenForOpponentsMoves(){
-    board.makeAMove(4);
-    board.makeAMove(4);
-    board.makeAMove(5);
-    board.makeAMove(4);
-    board.makeAMove(6);
-    board.makeAMove(4);
-    board.makeAMove(0);
-    board.makeAMove(2);
-    board.makeAMove(3);
-    board.makeAMove(5);
-    assertEquals(board.toString(),"14,13,1,3,2,1,13,13,12,15,-1\n"+
-                                  "3,13,13,2,0,3,3,14,14,10,4\nw");
-  }
-
-  @Test
-  public void makeAMovePassingOverATuzShouldEmptyIt(){
-    board.makeAMove(4);
-    board.makeAMove(4);
-    board.makeAMove(5);
-    board.makeAMove(4);
-    board.makeAMove(6);
-    board.makeAMove(4);
-    board.makeAMove(0);
-    board.makeAMove(2);
-    board.makeAMove(3);
-    assertEquals(board.toString(),"13,12,1,3,2,15,12,12,11,14,-1\n"+
-                                  "2,12,12,1,0,2,2,13,13,10,4\nb");
-  }
-
-  @Test
-  public void makeAMoveShouldNotCreateAtuzOnLastHole(){
-  board.makeAMove(8);
-  board.makeAMove(0);
-  board.makeAMove(1);
-  board.makeAMove(6);
-  board.makeAMove(1);
-  board.makeAMove(8);
-  assertEquals(board.toString(),"1,11,11,11,11,11,1,2,1,10,-1\n"+
-                                "2,2,13,12,12,12,12,12,3,12,-1\nw");
-
-  }*/
-
-  //@Test
-  /*public void makeAMove(){
-    board = new Board("0,0,0,0,0,0,0,0,0,13,0\n"+
-                      "0,0,1,0,0,0,0,0,0,20,0\nw");
-    board.makeAMove(2);
-    assertEquals(board.toString(),"0,0,0,0,0,0,0,0,0,13,0\n"+
-                                  "0,0,1,0,0,0,0,0,0,20,0\nw");
-  }*/
-
 }
